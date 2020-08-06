b0b5ef50df109fddf907fdb24af06d06e894c7ff
AikoMegumin <49574651+KobayashiLuna@users.noreply.github.com>
2020-08-06 13:15:59 -0300

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -6,5 +6,12 @@ module.exports = {
   aliases: ['pong'],
   description: 'pong!🏓',
   usage: '<só fazer o comando, rsrs>',
-  run: (client, message, args) => {}
+  run: (client, message, args) => {
+        const msg = await message.channel.send(`<a:ThinkLoad:684156228217208960> **Calculando o Ping...**`).then(
+        m => m.delete(10000));
+        //msg.edit
+        message.channel.send(`<a:Verification:684156238384463903> **Pong!**
+        > A latência é de: **${Math.floor(msg.createdAt - message.createdAt)}ms**
+        > A latência da API é de: **${Math.round(client.ping)}ms**`);
+    }
 };
