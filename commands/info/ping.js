e3b4e0d34fc4dec8dca0e7ebb0c15319a18e1dfc
AikoMegumin <49574651+KobayashiLuna@users.noreply.github.com>
2020-08-06 13:32:05 -0300

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -8,10 +8,10 @@ module.exports = {
   usage: '<só fazer o comando, rsrs>',
     run: async (client, message, args) => {
         message.delete();
-        const msg = await message.channel.send(`<a:ThinkLoad:684156228217208960> **Calculando o Ping...**`).then(
+        const msg = await message.channel.send(`<a:ThinkLoad:740966294060269638> **Calculando o Ping...**`).then(
         m => m.delete(10000));
         //msg.edit
-        message.channel.send(`<a:Verification:684156238384463903> **Pong!**
+        message.channel.send(`<a:Verification:740966298334527559> **Pong!**
         > A latência é de: **${Math.floor(msg.createdAt - message.createdAt)}ms**
         > A latência da API é de: **${Math.round(client.ping)}ms**`);
     }
