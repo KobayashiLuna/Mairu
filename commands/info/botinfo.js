d9e2ddca2774536f370f650669833a0016a6dd8c
AikoMegumin <49574651+KobayashiLuna@users.noreply.github.com>
2020-08-16 14:39:30 -0300

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -45,9 +45,9 @@ module.exports = {
             `
             **> 💬Meu nome no servidor:** Mairu
             **> 👑 Minha Dona: ** <@!698915459764060170>
-            **> ⚙️ Fui criada com ajuda e com os documentos dos Dollars ** Todos os direitos e agradecimentos a eles.
-            **> <:Dollars:744610073162154016> DOLLARS: ** Designer, Idea and Codding: <@!336311215099740160>\nCodding and Revision: <@!568923940768972808> & <@!570276100681236480>.
-            **> 📚 Docs: ** https://discordjs.guide/ | https://discord.js.org/ | **DOLLARS**.
+            **> ⚙️ Fui criada com ajuda e com os documentos dos Dollars ** Todos os direitos e agradecimentos a eles.\n\n
+            **> <:Dollars:744610073162154016> DOLLARS: ** Designer, Idea and Codding: <@!336311215099740160>\nCodding and Revision:\n<@!568923940768972808> & <@!570276100681236480>.
+            **> 📚 Docs: ** | https://discordjs.guide/ | https://discord.js.org/ | **DOLLARS** |.\n
             **> 🔮 Eu estou em:** **${client.guilds.size}** servidores!
             **> 📅 Fui criada em:** ${CriadoD} às ${CriadoH}
             `, true
