4911dd0ffc750fe1a40b9736f48d73815343420a
AikoMegumin <49574651+KobayashiLuna@users.noreply.github.com>
2020-08-16 14:43:20 -0300

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -43,7 +43,7 @@ module.exports = {
 
             .addField('💻 Minhas Informações:',
             `
-            **> 💬Meu nome no servidor:** Mairu
+            \n**> 💬Meu nome no servidor:** Mairu [Nome Padrão]
             **> 👑 Minha Dona: ** <@!698915459764060170>
             **> ⚙️ Fui criada com ajuda e com os documentos dos Dollars ** Todos os direitos e agradecimentos a eles.\n\n
             **> <:Dollars:744610073162154016> DOLLARS: ** Designer, Idea and Codding: <@!336311215099740160>\nCodding and Revision:\n<@!568923940768972808> & <@!570276100681236480>.
